2d55f35ee1b832b4756e4724a374219ab866d3bb
Taufan Augusta <taufan.augusta@protonmail.com>
2022-02-02 23:05:49 +0700

    test: HourlyTask

--- src/Application/Common/Managers/ApplicationManager.cs
+++ src/Application/Common/Managers/ApplicationManager.cs
@@ -1,9 +1,50 @@
-
+using Lomboque.Application.Common.Interfaces;
+using System.Net.Http;
 
 namespace Lomboque.Application.Common.Managers
 {
     public class ApplicationManager
     {
+        private readonly IRuntimeService runtimeService;
+        private readonly IHttpClientFactory clientFactory;
+
+        public ApplicationManager(IRuntimeService runtimeService, IHttpClientFactory clientFactory)
+        {
+            this.runtimeService = runtimeService;
+            this.clientFactory = clientFactory;
+        }
+
+        public async Task HourlyTask(string uri)
+        {
+            await runtimeService.RunInBackground(TimeSpan.FromSeconds(5), () => HttpRequest(uri).Wait());
+        }
         
+        private async Task HttpRequest(string uri)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, uri);
+            var client = clientFactory.CreateClient();
+            var response = await client.SendAsync(request);
+
+            if (response.IsSuccessStatusCode)
+            {
+                using var responseStream = await response.Content.ReadAsStreamAsync();
+                using var streamReader = new StreamReader(responseStream);
+
+                string json = streamReader.ReadToEnd();
+                System.Console.WriteLine(json);
+            }
+        }
     }
 }
+
+
+/*
+    private async Task RunInBackground(TimeSpan timeSpan, Action action)
+    {
+        var periodicTimer = new PeriodicTimer(timeSpan);
+        while (await periodicTimer.WaitForNextTickAsync())
+        {
+            action();
+        }
+    }
+*/
--- src/Application/DependencyInjection.cs
+++ src/Application/DependencyInjection.cs
@@ -1,6 +1,7 @@
 using Microsoft.Extensions.DependencyInjection;
 using FluentValidation;
 using Lomboque.Application.Actions;
+using Lomboque.Application.Common.Managers;
 
 namespace Lomboque.Application
 {
@@ -9,6 +10,7 @@ namespace Lomboque.Application
         public static IServiceCollection AddApplication(this IServiceCollection services)
         {
             services.AddScoped<AppController>();
+            services.AddSingleton<ApplicationManager>();
 
             return services;
         }
